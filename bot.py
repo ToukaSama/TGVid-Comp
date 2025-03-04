70ac001c339a5b689b2b364fe36296c77dbc0ad4
ToukaSama <blacksummoner6659@gmail.com>
2025-03-04 17:09:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,8 +37,8 @@ class Bot (Client):
         self.username = me.username
         app = web.AppRunner(await web_server())
         await app.setup()
-        bind_address = "0.0.0.0"
-        await web.TCPSite(app, bind_address, Config.0.0.0.0).start() 
+        bind_address = "8080"
+        await web.TCPSite(app, bind_address, Config.PORT).start() 
         
         logging.info(f"✅ {me.first_name} with for Pyrogram v{__version__} (Layer {layer}) started on {me.username}. ✅")
 
